867f408edb149dd59c358ee00cc63f8eaa6635ff
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2020-07-06 13:53:50 -0400

    Create get_data.js

---
+++ js/get_data.js
@@ -0,0 +1,12 @@
+function getDataFromURL(url) {
+  const _d = require(url).split('\n').map(i => i.split(','));
+  let d = [];
+  for(let i=1; i<_d.length; i++) {
+    let __d = {};
+    for(let j in _d[i]) {
+      __d[_d[0][j]] = _d[i][j];
+    }
+    d.push(__d);
+  }
+  return d;
+}
