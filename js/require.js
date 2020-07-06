3ccac5fd207f4d1a34b505bd963e49b7e313dbbc
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2020-07-06 13:53:22 -0400

    Create require.js

---
+++ js/require.js
@@ -0,0 +1,20 @@
+function require(url) {
+  let rtrn;
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.onload = e => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr);
+        rtrn = xhr.responseText;
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.onerror = e => {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null);
+  return rtrn;
+}
