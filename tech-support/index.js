67c048fe50227b5262fcb463879ce76e90c0f02e
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2020-07-06 13:25:56 -0400

    Create index.js

---
+++ tech-support/index.js
@@ -0,0 +1,14 @@
+document.getElementById('txt').addEventListener("keydown", e => {
+  if(e.key=="Enter") {
+    search(document.getElementById('txt').value);
+  }
+});
+
+function triggerSearch(s) {
+  document.getElementById("txt").value = s;
+  let event = document.createEvent("Event");
+  event.initEvent("keydown",true,true);
+  event.key = "Enter";
+  document.getElementById('txt').dispatchEvent(event);
+}
+
