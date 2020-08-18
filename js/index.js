ab789cf37589249ad57374f1ec8f5a78b81b37e8
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2020-08-18 18:53:45 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ import { createTrigger } from "./ai.js"
 import { enableVoiceSearch } from "./voice_search.js"
 
 
-export default function startAI {
-  this.data = get(url);
-  this.search = s => search(s, this.data);
-  enableVoiceSearch(createTrigger(this.search));
+export default function startAI(url) {
+  const data = get(url);
+  const search = s => search(s, data);
+  enableVoiceSearch(createTrigger(search));
 }
