093e3d592be944a986219c27e558384e8002842c
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2020-08-18 18:49:56 -0400

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,13 +1,16 @@
-document.getElementById('txt').addEventListener("keydown", e => {
-  if(e.key=="Enter") {
-    search(document.getElementById('txt').value);
-  }
-});
+export function createTrigger(search) {
+  document.getElementById('txt').addEventListener("keydown", e => {
+    if(e.key=="Enter") {
+      search(document.getElementById('txt').value);
+    }
+  });
 
-function triggerSearch(s) {
-  document.getElementById("txt").value = s;
-  let event = document.createEvent("Event");
-  event.initEvent("keydown",true,true);
-  event.key = "Enter";
-  document.getElementById('txt').dispatchEvent(event);
+  function triggerSearch(s) {
+    document.getElementById("txt").value = s;
+    let event = document.createEvent("Event");
+    event.initEvent("keydown",true,true);
+    event.key = "Enter";
+    document.getElementById('txt').dispatchEvent(event);
+  }
+  return triggerSearch;
 }
